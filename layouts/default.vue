302cb02237fc7dd117257bce7b0a8f8f8c3afcd7
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-18 17:06:35 -0300

    feat: created default layout and index page :sparkles:

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<template>
+  <main class="container mx-auto px-4 pt-4 lg:px-8">
+    <div class="flex flex-wrap relative">
+      <slot />
+    </div>
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLayout name="default">
+    <main class="container mx-auto px-4 pt-4 lg:px-8">
+      <div class="flex flex-wrap relative">
+        <p>Home</p>
+      </div>
+    </main>
+  </NuxtLayout>
+</template>
