a5cf1d194669e5f48f7fc3176f2eca7b3a0a756f
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-18 16:45:48 -0300

    build: add windicss :heavy_plus_sign:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: [
+    'virtual:windi.css',
+    'virtual:windi-devtools',
+  ],
+  modules: [
+    'nuxt-windicss',
+  ],
   typescript: {
     strict: true
   }
