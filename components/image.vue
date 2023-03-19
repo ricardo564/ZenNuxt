4132ea6dbd13e0f0bea835ff803bd0c143b8e471
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-19 09:27:49 -0300

    chore: add configuration props to Image :zap:

--- components/image.vue
+++ components/image.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { defineProps } from "vue";
-
 const props = defineProps({
   src: {
     type: String,
@@ -10,6 +8,29 @@ const props = defineProps({
     type: String,
     required: true,
   },
+  width: {
+    type: Number,
+    default: 272,
+  },
+  height: {
+    type: Number,
+  },
+  preload: {
+    type: Boolean,
+    default: true,
+  },
+  fit: {
+    type: String,
+    default: "cover",
+  },
+  format: {
+    type: String,
+    default: "auto",
+  },
+  quality: {
+    type: Number,
+    default: 75,
+  },
 });
 
 const { src, alt } = props;
@@ -18,6 +39,12 @@ const { src, alt } = props;
   <nuxt-img
     class="h-auto mx-auto object-cover object-center w-full transform transition ease-in-out duration-500 hover:shadow-xl hover:z-50 hover:scale-110 hover:-translate-y-1 hover:translate-x-1"
     loading="lazy"
+    :preload="preload"
+    :fit="fit"
+    :format="format"
+    :quality="quality"
+    :width="width"
+    :height="height"
     :src="src"
     :alt="alt"
   />
