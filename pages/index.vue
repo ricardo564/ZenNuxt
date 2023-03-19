4c678f0ce18fff9a5680f3414f29071bc8210812
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-19 10:16:01 -0300

    fix: add section on div to avoid duplicate content on build :recycle:

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,30 @@
 <template>
   <NuxtLayout name="default">
-    <main class="h-auto mx-auto w-full grid px-4 pt-4 lg:px-8">
-      <div class="grid relative">
-        <HomeIntro />
-        <HomeProjects />
-        <HomeClients />
-        <HomeReviews />
-        <HomeContact />
+    <main
+      class="h-auto mx-auto w-full grid px-4 pt-4 lg:px-8"
+    >
+      <div
+        class="grid relative"
+      >
+        <div>
+          <HomeIntro />
+        </div>
+        <div>
+          <HomeProjects />
+        </div>
+        <div>
+          <HomeClients />
+        </div>
+        <div>
+          <HomeReviews />
+        </div>
+        <div>
+          <HomeContact />
+        </div>
+        <div>
+          <HomeFooter />
+        </div>
       </div>
-      <HomeFooter />
     </main>
   </NuxtLayout>
 </template>
